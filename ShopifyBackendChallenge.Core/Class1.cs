0b36199ab3e9198d58becb6e399564bb09e5058f
Daniel Butts <buttsda@sheridancollege.ca>
2021-09-14 19:03:03 -0400

    Add project files.

---
+++ ShopifyBackendChallenge.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShopifyBackendChallenge.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ ShopifyBackendChallenge.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShopifyBackendChallenge.Data
+{
+    public class Class1
+    {
+    }
+}
