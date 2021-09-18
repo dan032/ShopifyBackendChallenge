ee70c8ad52d501b684572da2681d8067ec21c293
Daniel Butts <buttsda@sheridancollege.ca>
2021-09-17 23:07:49 -0400

    Fixed issue where tests werent added to git.

---
+++ ShopifyBackendChallenge.Tests/Image/ImageControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ShopifyBackendChallenge.Core.User;
+using ShopifyBackendChallenge.Data;
+using ShopifyBackendChallenge.Data.Common;
+using ShopifyBackendChallenge.Data.FileStorage;
+using ShopifyBackendChallenge.Data.SqlServer;
+using ShopifyBackendChallenge.Data.Utils;
+using ShopifyBackendChallenge.Tests.Utils;
+using ShopifyBackendChallenge.Web.Controllers;
+using ShopifyBackendChallenge.Web.Helpers;
+using ShopifyBackendChallenge.Web.Models;
+using ShopifyBackendChallenge.Web.Services.common;
+using ShopifyBackendChallenge.Web.Services.Jwt;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopifyBackendChallenge.Tests.Image
+{
+    public class ImageControllerTests : IClassFixture<SharedDatabaseFixture>
+    {
+        public ImageControllerTests(SharedDatabaseFixture fixture) => Fixture = fixture;
+
+        public SharedDatabaseFixture Fixture { get; }
+
+        [Fact]
+        public async void ImageController_UploadImage_Successful()
+        {
+            using (var transaction = Fixture.Connection.BeginTransaction())
+            {
+                using (var context = Fixture.CreateContext(transaction))
+                {
+                    IUserData userData = new SqlUserData(context);
+                    var appSettings = Options.Create(new AppSettings
+                    {
+                        Secret = "Daasdfsadadssadadsasddasasfdffsadn"
+                    });
+
+                    IImageMetadata imageMetadata = new SqlImageMetadata(context);
+                    IImageRepo imageRepo = new FolderStorageImageRepo();
+                    var controller = new ImagesController(imageRepo, imageMetadata);
+
+                    var image = File.OpenRead(@"..\..\..\TestImages\test.jpg");
+
+                    var formData = new FormFile(image, 0, 0, "test", "test.jpg");
+
+
+                    SingleImageModel singleImageModel = new SingleImageModel
+                    {
+                        Image = formData,
+                        Description = "Test",
+                        Title = "Test"
+                    };
+                    controller.ControllerContext.HttpContext = new DefaultHttpContext();
+                    controller.HttpContext.Items["User"] = new UserModel
+                    {
+                        Id = 1,
+                        Username = "test",
+                        Hash = "hash",
+                        Salt = "salt"
+                    };
+
+                    IActionResult actionResult = await controller.PostImage(singleImageModel);
+                    var okResult = actionResult as OkObjectResult;
+                    Assert.Equal(200, okResult.StatusCode);
+                }
+            }
+        }
+    }
+}
---
+++ ShopifyBackendChallenge.Tests/User/UserTests.cs
@@ -0,0 +1,55 @@
+﻿using ShopifyBackendChallenge.Core.User;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Xunit;
+
+namespace ShopifyBackendChallenge.Tests.User
+{
+    public class UserTests
+    {
+        [Fact]
+        public void User_NewUserValidProperties_Valid()
+        {
+            UserModel user = new UserModel
+            {
+                Username = "danbutts",
+                Hash = "hash-goes-here",
+                Salt = "salt-goes-here"
+            };
+
+            ValidationContext context = new ValidationContext(user);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool validUser = Validator.TryValidateObject(user, context, results, true);
+            Assert.True(validUser);
+        }
+
+        [Fact]
+        public void User_NewUserUsernameTooLong_Invalid()
+        {
+            UserModel user = new UserModel
+            {
+                Username = "danbutasdasdasdadxasfdsddsafsdjklasdfnkohnoksfdanofsadnkfsdanksdfnkjfdsalndslfaknlkdfsnjdfts",
+                Hash = "hash-goes-here",
+                Salt = "salt-goes-here"
+            };
+
+            ValidationContext context = new ValidationContext(user);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool validUser = Validator.TryValidateObject(user, context, results, true);
+            Assert.False(validUser);
+        }
+
+        [Fact]
+        public void User_NewUserMissingProperties_Invalid()
+        {
+            UserModel user = new UserModel();
+
+            ValidationContext context = new ValidationContext(user);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool validUser = Validator.TryValidateObject(user, context, results, true);
+            Assert.False(validUser);
+        }
+    }
+}
